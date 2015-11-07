e01aa17ac28df1e4dfe0cc00e4a49e35efd1fb17
Hout <houtzager@gmail.com>
2015-11-07 13:41:26 +0100

    Added Tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -400,45 +400,6 @@ class JHDateSpec: QuickSpec {
             }
         }
 
-        context("equations") {
-
-            it("should return true for equating a different object with the same properties") {
-                let date1 = JHDate(year: 1999, month: 12, day: 31)!
-                let date2 = date1
-
-                expect(date1 == date2) == true
-            }
-            
-            it("should return true for equating the same object") {
-                let date1 = JHDate(year: 1999, month: 12, day: 31)!
-                let date2 = date1
-
-                expect(date1 == date2) == true
-            }
-            
-            it("should return false for equating objects with different dates") {
-                let date1 = JHDate(year: 1999, month: 12, day: 31)!
-                let date2 = JHDate(year: 1999, month: 12, day: 30)!
-
-                expect(date1 == date2) == false
-            }
-            
-            it("should return false for equating objects with different calendars") {
-                let date1 = JHDate(year: 1999, month: 12, day: 31, calendar: NSCalendar(identifier: NSCalendarIdentifierGregorian))!
-                let date2 = JHDate(refDate: date1, calendar: NSCalendar(identifier: NSCalendarIdentifierChinese))
-
-                expect(date1 == date2) == false
-            }
-            
-            it("should return false for equating objects with different locales") {
-                let date1 = JHDate(year: 1999, month: 12, day: 31, locale: NSLocale(localeIdentifier: "en_UK"))!
-                let date2 = JHDate(refDate: date1, locale: NSLocale(localeIdentifier: "en_US"))
-
-                expect(date1 == date2) == false
-            }
-            
-        }
-
         context("comparisons") {
 
             it("should return true for greater than comparing") {
